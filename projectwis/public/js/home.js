b1d6f0eb810518ae8269eacb72c037e97321a1fa
Jose Eduardo <joseeduardoqueiros5@gmail.com>
2022-10-23 13:49:58 -0300

    attBtnFechar

--- projectwis/public/js/home.js
+++ projectwis/public/js/home.js
@@ -16,5 +16,6 @@ btnExcluir.addEventListener('click', ()=>{
 var btnFechar = document.getElementById('Fechar');
 
 btnFechar.addEventListener('click', ()=>{
-  window.location.href = "/"
+  //window.location.href = "/"
+  document.getElementById('mostraAtividade').style.display="none";
 })
